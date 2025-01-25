e680902a7cdcf3fdb202f94e77b036b95e9bbcb5
Chloé Cilia <chloe.cilia68@gmail.com>
2025-01-25 22:38:06 +0100

    empty commit

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface
 {
     #[ORM\Id]
@@ -27,6 +29,9 @@ class User implements UserInterface
 
     #[ORM\Column(type: 'json')]
     private array $roles = [];
+
+    #[ORM\Column]
+    private bool $isVerified = false;
 
     // Getter for ID
     public function getId(): ?int
@@ -103,5 +108,17 @@ class User implements UserInterface
     public function eraseCredentials(): void
     {
         // Optional: erase sensitive data after authentication
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
     }
 }
